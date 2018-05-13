1346edae4c2c853e639fbb5ca607ed8d8ae29d93
nicolasrivadeneira <andres_mar08@hotmail.com>
2018-05-13 18:16:09 -0300

    Termina con la interfaz, falta linkear

--- src/interfaz/TableroGUI.java
+++ src/interfaz/TableroGUI.java
@@ -9,37 +9,42 @@ import model.TipoSuelo;
 import utils.Matriz;
 
 public class TableroGUI extends javax.swing.JPanel {
-    private static ImageIcon comun, pared, obstaculo, pasto, puerta_abierta, puerta_cerrada, alfombra, agua, arena, basura, ropa, escalera_n, escalera_s, escalera_e, escalera_o, agente, ninio;
-//	private ImageIcon agua, tocado;
-    private boolean tipoTablero;
+    private static ImageIcon comun, pared, obstaculo, pasto, puerta_abierta, puerta_cerrada, alfombra, agua, arena, basura, ropa, escalera_n, escalera_s, escalera_e, escalera_o, agente, ninio, oculto;
     private CasillasGUI [][] casillas ;
+    private int anchoCasilla;
+    private char [] pisosAccesibles = {'_', '~','a', '\'', '!', '%', '*', '&', 'N', 'S', 'E', 'O', 'A', 'B'};
     
 //    public TableroGUI() {
 //        initComponents(cantFilas, cantCol);
 //    }
-    public TableroGUI(boolean tipo, char [][] plano) {
+    public TableroGUI(char [][] plano, PrincipalNueva principalNueva, int anchoPanel, int altoPanel) {
         int cantFilas = plano.length;
         int cantCol = plano[0].length;
+        anchoCasilla = Math.min(anchoPanel/cantCol, altoPanel/cantFilas);
     	
         initComponents(cantFilas, cantCol);
-        int x,y;
-        setLayout(new java.awt.GridLayout(cantFilas, cantCol));
-        this.tipoTablero = false;//tipo;
+        setLayout(new GridLayout(cantFilas, cantCol));
         cargarImagenes();
-        casillas = new CasillasGUI[cantFilas][cantCol];
+        generarCasillas(cantFilas, cantCol, plano, principalNueva);
+    }
+    
+    private void generarCasillas(int cantFilas, int cantCol, char[][] plano, PrincipalNueva principalNueva){
+        int x,y;
+    	casillas = new CasillasGUI[cantFilas][cantCol];
         for (int i = 0; i < cantFilas; i++){
             for (int j = 0; j < cantCol; j++){
-                casillas[i][j] = new CasillasGUI(this);
-                casillas[i][j].setFondo(seleccionarFondo(plano[i][j]));                
-                x = (i * 35)+1;
-                y = (j * 35)+1;
-                casillas[i][j].setBounds(x, y, 34, 34);
+                casillas[i][j] = new CasillasGUI(this, principalNueva);
+                casillas[i][j].setFondo(seleccionarFondo(plano[i][j]));
+                casillas[i][j].setAccesible(esAccesible(plano[i][j]));
+                y = (i * (anchoCasilla+1))+1;
+                x = (j * (anchoCasilla+1))+1;
+                casillas[i][j].setBounds(x, y, anchoCasilla, anchoCasilla);
                 this.add(casillas[i][j]);
             }
         }
     }
-    
-    private static ImageIcon seleccionarFondo (char caracter){
+
+	private static ImageIcon seleccionarFondo (char caracter){
 
 		ImageIcon imagen = null;
 		
@@ -95,6 +100,9 @@ public class TableroGUI extends javax.swing.JPanel {
 		case 'B':
 			imagen=ninio;
 			break;
+		case 'X':
+			imagen=oculto;
+			break;
 
 		default:
 			break;
@@ -102,15 +110,42 @@ public class TableroGUI extends javax.swing.JPanel {
 
 		return imagen;
 	}
-    
-    public boolean getTipoTablero(){
-        return this.isTipoTablero();
+	
+    private boolean esAccesible(char c) {
+    	for (int i = 0; i < pisosAccesibles.length; i++) {
+			if (c == pisosAccesibles[i]){
+				return true;
+			}
+		}
+    	return false;
     }
     
-//    public void pintar(int x, int y){
-//        this.casillas[x][y].setFondo(tocado);
-//        this.repaint();
-//    }
+    
+    public void pintar(int x, int y, int i){
+    	ImageIcon fondo = null;
+    	if (i==1){
+    		fondo=agente;
+    	} else if (i==2){
+    		fondo=ninio;
+    	}
+        this.casillas[x][y].setFondo(fondo);
+        this.repaint();
+    }
+    
+    public ImageIcon obtenerFondo(int x, int y){
+
+    	return casillas[x][y].getFondo();
+    }
+    
+    public void setearFondo(int x, int y, ImageIcon fondoBackUp){
+
+    	casillas[x][y].setFondo(fondoBackUp);
+    }   
+    
+    public void setearAccesible(int x, int y, Boolean bool){
+
+    	casillas[x][y].setAccesible(bool);
+    }
     
     private void cargarImagenes() {
       this.comun = this.cargarFondo("comun.gif");
@@ -130,6 +165,7 @@ public class TableroGUI extends javax.swing.JPanel {
       this.escalera_o = this.cargarFondo("escalera_o.gif");
       this.agente = this.cargarFondo("agente.gif");
       this.ninio = this.cargarFondo("ninio.gif");
+      this.oculto = this.cargarFondo("oculto.gif");
     }
 
     protected static ImageIcon cargarFondo(String ruta) {
@@ -144,11 +180,11 @@ public class TableroGUI extends javax.swing.JPanel {
     
     public int[] getCoordenadas(CasillasGUI casilla) {
         int [] coordenadas = new int[2];
-        for (int i=0; i < this.casillas.length; i++) {
-            for (int j=0; j < this.casillas.length; j++) {
-                if (this.casillas[i][j] == casilla) {
-                    coordenadas[0] = i;
-                    coordenadas[1] = j;
+        for (int y=0; y < this.casillas.length; y++) {
+            for (int x=0; x < this.casillas[0].length; x++) {
+                if (this.casillas[y][x] == casilla) {
+                    coordenadas[0] = x;
+                    coordenadas[1] = y;
                 }
             }
         }
@@ -162,19 +198,12 @@ public class TableroGUI extends javax.swing.JPanel {
     public void setCasillas(CasillasGUI[][] casillas) {
         this.casillas = casillas;
     }
-    
-    public boolean isTipoTablero() {
-        return tipoTablero;
-    }    
-    public void setTipoTablero(boolean tipoTablero) {
-        this.tipoTablero = tipoTablero;
-    }
                               
     private void initComponents(int cantFilas, int cantCol) {
         setLayout(null);
         setBackground(new java.awt.Color(0, 0, 0));
         setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        setPreferredSize(new java.awt.Dimension(cantFilas*35, cantCol*35)); //(351, 351)); //35 cada una
+        setPreferredSize(new java.awt.Dimension(cantCol*(anchoCasilla+1), cantFilas*(anchoCasilla+1))); //(351, 351)); //35 cada una
     }                      
                      
 }
